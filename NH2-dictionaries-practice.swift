a0f2127fa57dc08e01f93c0f309ca057095be5a8
Kayley S <47701395+kayleyseow@users.noreply.github.com>
2021-07-12 07:33:04 +0000

    Upload code from Week 1 Tuesday Night Hack (NH2-dictionaries-practice.swift) 6-22-2021

---
+++ NH2-dictionaries-practice.swift
@@ -0,0 +1,16 @@
+//⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️
+// In your own words, how would you define a Swift dictionary?
+// A dictionary is much like a variable conaining variables inside it which links to its own specific qualities.
+
+// If you wanted to have a program that keeps track of all the school/office supplies that a person has, would you use a dictionary or array? Explain
+//I would use a dictionary because this data does not need to be stored in any specific order.
+
+// Now, declare a variable that store the dictionary or array for the question above!
+ let schoolSupplies = ["pencil(s)":10, "pen(s)": 5, "ream(s) of paper":3]
+
+//⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️⭐️
+//Declare a new variable that holds a dictionary. You can choose the variable name and what type of data the dictionary holds. Your dictionary needs to have at least 5 key/value pairs
+let plantsInGarden = ["Rose bush(es)":7, "Lotus(es)":1, "Cactus plant(s)":15, "Strawberry plant(s)":4, "Green Onion":30]
+print(plantsInGarden)
+
+// Used a dictionary for the school supplies. You answered that you would use a dictionary because it does not need to be in a specific order which is correct. An array may also be used if you do not need to track the quantity of each item, which is what you did :) Scored exemplary.
